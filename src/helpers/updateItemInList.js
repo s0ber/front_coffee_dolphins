1d1f0320431b43a0e6acf88aaf753f5513e1a33d
Sergey <sergeyshishkalov@gmail.com>
2016-01-26 23:57:50 +0300

    Filter out some protected attributes

--- src/helpers/updateItemInList.js
+++ src/helpers/updateItemInList.js
@@ -1,11 +1,12 @@
 import findItemById from './findItemById'
+const PROTECTED_ATTRIBUTES = {password: null, password_confirmation: null}
 
 export default function(state, item, attributes) {
   const itemData = findItemById(state, item.id)
   const index = itemData.index
 
   if (index !== -1) {
-    item = Object.assign({}, itemData.item, item, attributes)
+    item = Object.assign({}, itemData.item, item, attributes, PROTECTED_ATTRIBUTES)
 
     return [
       ...state.slice(0, index),
