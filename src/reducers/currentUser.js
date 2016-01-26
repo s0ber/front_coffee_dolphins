9aebc5da5ac6fac512f747d7c59287382cdba8c2
Sergey <sergeyshishkalov@gmail.com>
2016-01-26 23:27:50 +0300

    Update current user in the interface, if it was updated

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,4 +1,4 @@
-import {LOGIN_USER, LOGOUT_USER, SET_CURRENT_USER} from 'actions'
+import {LOGIN_USER, LOGOUT_USER, SET_CURRENT_USER, UPDATE_USER} from 'actions'
 const Anonymous = {id: 0, role: 'Anonymous'}
 
 export default function(state = null, action = {}) {
@@ -9,6 +9,12 @@ export default function(state = null, action = {}) {
       return Anonymous
     case SET_CURRENT_USER:
       return action.payload || Anonymous
+    case UPDATE_USER:
+      if (action.payload.id == state.id) {
+        return Object.assign({}, state, action.payload)
+      } else {
+        return state
+      }
     default:
       return state
   }
